9fc1c3e0873db48edb5da1def9f196a9bc13826a
tg <tg@tg.local>
2016-08-23 12:18:32 -0400

    Style about page and link to resources

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,26 @@ var React = require('react');
 var About = React.createClass({
   render: function() {
     return (
-      <h3>About Component</h3>
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>
+          This is a weather app built using React.
+        </p>
+        <p>
+          Here are some of the tools I used:
+        </p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">OpenWeatherMap</a> - This was the weather API used to search for weather data by city name.
+          </li>
+          <li>
+            <a href="https://foundation.zurb.com">Foundation</a> - This was the front-end framework used.
+          </li>
+        </ul>
+      </div>
     );
   }
 });
